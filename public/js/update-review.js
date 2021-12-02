613fa6eb6e7efe2d693c187cfcfd7013cb90bbe8
dookebroyle <57194791+dookebroyle@users.noreply.github.com>
2021-12-02 14:20:38 -0700

    corrected bug in update-review.js that was only selecting first update form

--- public/js/update-review.js
+++ public/js/update-review.js
@@ -1,30 +1,32 @@
-const updateForm = document.querySelector('#update-form')
+const updateForms = document.querySelectorAll('.update-form')
 const successText = document.querySelector('#success-text')
 
 //listen for post click on Review Update form and fire off ratings/postupdate
-updateForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    let bookTitle = document.querySelector('#booktitle').value
-    let stars = document.querySelector('input[name="rate"]:checked').value;
-    let input = document.querySelector('#review-input').value
+updateForms.forEach( (form) => {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        let bookTitle = document.querySelector('#booktitle').value
+        let stars = document.querySelector('input[name="rate"]:checked').value;
+        let input = document.querySelector('#review-input').value
 
-    fetch(`http://localhost:3000/ratings/postupdate`, {
-        method: "PATCH",
-        headers: {
-            "content-type" : "application/json",
-        },
-        redirect: 'follow',
-        body: JSON.stringify({
-            bookTitle,
-            stars, 
-            input
+        fetch(`http://localhost:3000/ratings/postupdate`, {
+            method: "PATCH",
+            headers: {
+                "content-type" : "application/json",
+            },
+            redirect: 'follow',
+            body: JSON.stringify({
+                bookTitle,
+                stars, 
+                input
+            })
+        }).then((response) => {
+            if (response.error){
+                console.log(response.error)
+            } else {
+                successText.innerHTML = 'Review Updated!!'
+            }
         })
-    }).then((response) => {
-        if (response.error){
-            console.log(response.error)
-        } else {
-            successText.innerHTML = 'Review Updated!!'
-        }
     })
 })
 
