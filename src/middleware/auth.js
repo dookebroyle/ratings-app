7157b981567ad295f8a58477912b2ca48c4a9c07
dookebroyle <57194791+dookebroyle@users.noreply.github.com>
2022-01-09 07:54:54 -0700

    attempting to fix jwtsecret bug

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,7 +3,7 @@ const User = require('../models/user')
 //const { jwtsecret } = require('../config.js');
 
 require('dotenv').config()
-const jwtsecret = process.env.JWT_SECRET
+
 
 
 //verify user is logged in
@@ -11,7 +11,7 @@ const auth = async (req, res, next) => {
         try {
             // retrieve user authorization from HTTPonly cookie with token
             const token = req.cookies.Authorization.replace('Bearer ', '')
-            let decoded = jwt.verify(token, jwtsecret)
+            let decoded = jwt.verify(token, 'jwtsecret')
             decoded = Object.values(decoded);
             const decodedValue = decoded[0];
             const user = await User.findOne({_id: decodedValue, 'tokens.token':token})
