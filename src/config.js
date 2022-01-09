bb3372206aaae170954cf1788ffd122143357501
dookebroyle <57194791+dookebroyle@users.noreply.github.com>
2022-01-09 07:32:11 -0700

    commented out config file

--- src/config.js
+++ src/config.js
@@ -1,18 +1,18 @@
-// config.js
-const dotenv = require('dotenv');
-const result = dotenv.config();
+// //config.js
+// const dotenv = require('dotenv');
+// const result = dotenv.config();
 
-if (result.error) {
-  throw result.error
-}
+// if (result.error) {
+//   throw result.error
+// }
 
-//these are defined in .env file. Make up jwtsecret. get NYTimes API key from site.
-//mongoDB URL can either be Mongo Atlas path or local db
-//require('../config.js') in all files that use
-module.exports = {
-  masterkey: process.env.NYTIMES_API_KEY,
-  port: process.env.PORT,
-  mongodburl: process.env.MONGODB_URL,
-  jwtsecret: process.env.JWT_SECRET,
-}
+// //these are defined in .env file. Make up jwtsecret. get NYTimes API key from site.
+// //mongoDB URL can either be Mongo Atlas path or local db
+// //require('../config.js') in all files that use
+// module.exports = {
+//   masterkey: process.env.NYTIMES_API_KEY,
+//   port: process.env.PORT,
+//   mongodburl: process.env.MONGODB_URL,
+//   jwtsecret: process.env.JWT_SECRET,
+// }
 
